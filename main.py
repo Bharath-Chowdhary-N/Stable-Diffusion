3936590475e1b5fb506a5a67e0ce996d52e7c0de
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-10-08 12:06:53 -0500

    description to main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # To model images, we need joint distributions of parameters. 
 # Our goal is to model complex distribution and sample from them.
+
+# Classifier free guidance: Unet given an image and time step (t)
+                     #--> In addition now prompt is also given as condition
+                     # we train a single network, at some probablity we set conditioned signal to zero.
+                     # So sometimes we get a conditioned output and sometimes we get an unconditioned output and we combine the weight to indicate how much we want the network to pay attention to the conditioning signal
+                     #  

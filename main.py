6c9690b414e6429cb8b150128b44371286a267be
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-10-08 13:04:40 -0500

    added descp

--- main.py
+++ main.py
@@ -5,4 +5,5 @@
                      #--> In addition now prompt is also given as condition
                      # we train a single network, at some probablity we set conditioned signal to zero.
                      # So sometimes we get a conditioned output and sometimes we get an unconditioned output and we combine the weight to indicate how much we want the network to pay attention to the conditioning signal
-                     #  
+
+# Clip model: Combines image with text

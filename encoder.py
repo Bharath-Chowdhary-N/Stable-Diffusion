af6a95255720ce6fb4610d02eec358287c839e27
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-12-20 15:14:37 -0600

    finished encoder part

--- encoder.py
+++ encoder.py
@@ -37,9 +37,27 @@ class VAE_encoder(nn.Sequential):
         #(Batch_size, 512, h/8, w/8) --> (Batch_size, 8, h/8, w/8)   
         nn.Conv2d(512,8, kernel_size=3,padding=1) ,
         nn.Conv2d(8, 8, kernel_size=1, padding=0)
+        )
     
-    def forward(self, x: torch.Tensor):
-        pass
+    def forward(self, x: torch.Tensor, noise: torch.Tensor):
+        #x Input shape: (Batch_size, Channel, H, W) 
+        #Noise: same as output shape of inititalization (Batchsize, Channel, H/8, W/8)
+        for module in self:
+            if getattr(module, stride, None) == (2,2):
+               x = F.pad(x, (0,1,0,1))
+            x = module(x)
         
+        mean , log_variance = torch.chunk(x,2,dim=1) #2x(Batch_size, 4, h/8, w/8)
 
+        log_variance = torch.clamp(log_variance, -30, 20)
 
+        variance = log_variance.exp()
+
+        std_dev = variance.sqrt()
+
+        x = mean + std_dev*noise
+
+        x *= 0.18215 # same constant used in paper
+
+        return x
+        

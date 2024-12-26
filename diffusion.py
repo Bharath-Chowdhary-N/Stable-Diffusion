c7ef0737c5e5801cc8a9122956b9eb77d0bd6ee8
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-12-25 18:38:07 -0600

    updated time embedding

--- diffusion.py
+++ diffusion.py
@@ -4,5 +4,17 @@ from torch.nn import Functional as F
 
 class Diffusion(nn.Module):
     def __init__(self):
+        super().__init__()
         pass
 
+class time_embedding(nn.Module):
+    def __init__(self, num_embed):
+        super().__init__()
+        self.linear_layer_1 = nn.Linear(num_embed, 4*num_embed)
+        self.linear_layer_2 = nn.Linear(4*num_embed, 4*num_embed)
+
+    def forward(self, x:torch.tensor) -> torch.tensor:
+        x = self.linear_layer_1(x)
+        x = F.Silu(x)
+        x=self.linear_layer_2(x)
+        return x

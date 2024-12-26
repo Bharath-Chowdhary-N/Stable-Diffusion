3ccb7e0e86fd913ac336cd17e9558f31d59385ba
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-12-25 22:14:27 -0600

    added UNET residual block

--- diffusion.py
+++ diffusion.py
@@ -19,4 +19,45 @@ class time_embedding(nn.Module):
         x=self.linear_layer_2(x) #input x: [1, num_embded] output x: [1, 4*num_embed]
         return x
 
+class UNET_residual_block(nn.Module):
+    def __init__(self, in_channel, out_channel, num_time_embed):
+        super().__init__()
+        #Build same as VAE residual block, except we are adding a time layer here
+        self.num_groups = 32
+        self.group_norm_1 = nn.GroupNorm(self.num_groups, in_channel)
+        self.conv_layer_1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=3, padding=1)
+
+        self.time_layer = nn.Linear(num_time_embed, out_channel)
+
+        self.group_norm_2 = nn.GroupNorm(self.num_groups, out_channel)
+        self.conv_layer_2 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=3, padding=1)
+
+        if in_channel == out_channel:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, padding=0)
+    
+    def forward(self, x, time_embed):
+        residue = x
+
+        x = self.group_norm_1(x)
+        x = F.Silu(x)
+        x = self.conv_layer_1(x)
+
+        time_embed = F.Silu(time_embed) 
+        time_embed = self.time_layer(time_embed) #this will be converted into (1, time_embed_shape) into (1, out_channel)   
+        x += time_embed.unsqueeze(-1).unsqueeze(-1)
+
+        x = self.group_norm_2(x)
+        x = F.Silu(x)
+        x = self.conv_layer_2(x)
+         
+        return x + self.residual_layer(residue)
+
+
+
+
+
+
+
 

0a06d88db6dc2875a59b6076284e24da96672bb5
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2024-12-26 02:00:11 -0600

    added cross_attention

--- attention.py
+++ attention.py
@@ -3,7 +3,7 @@ from torch import nn
 from torch.nn import functional as F
 import math
 
-class SelfAttention():
+class SelfAttention(nn.Module):
     """
     Input = (seq, d_model) # seq     --> sequence length (implies sequence of words / input params)
                            # d_model --> latent representation (typically in paper referred as 512)
@@ -54,3 +54,57 @@ class SelfAttention():
         output =self.W_output(attention_qkv)
 
         return output 
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads:int, d_model:int, d_cross:int):
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.d_cross = d_cross
+        self.W_input_q = nn.Linear(d_model, d_model)
+        self.W_input_k = nn.Linear(d_cross, d_model)
+        self.W_input_v = nn.Linear(d_cross, d_model)
+        self.W_output  = nn.Linear(d_model, d_model) 
+        self.d_k = self.d_model/self.n_heads
+
+        assert isinstance(self.d_k, int)
+        
+    def forward(self, x, y):
+        """
+        x: (batch_size, seq_q, d_model)
+        y: (batch_size, seq_kv, d_cross)
+        """
+        batch_size, seq_q, d_model = x.shape
+        
+        interim_dim = (batch_size, -1, self.n_heads, self.d_k) #splitting the input into n_heads
+
+        q_dash = self.W_input_q(x)
+        k_dash = self.W_input_k(y)
+        v_dash = self.W_input_v(y)
+
+        q_dash = q_dash.view(interim_dim).transpose(1,2) #(batch_size, self.n_heads, seq_q, self.d_k)
+        k_dash = k_dash.view(interim_dim).transpose(1,2) #(batch_size, self.n_heads, seq_kv, self.d_k)
+        v_dash = v_dash.view(interim_dim).transpose(1,2) #(batch_size, self.n_heads, seq_kv, self.d_k)
+
+
+        qk_product = q_dash @ k_dash.transpose(-1,-2) #(batch_size, self.n_heads, seq_q, seq_kv)
+
+        qk_product /= math.sqrt(self.d_k)
+
+        qk_product = F.softmax(qk_product, dim=-1)
+
+        attention_qkv = qk_product @ v_dash #(batch_size, self.n_heads, seq_q, d_k)
+
+        attention_qkv = attention_qkv.transpose(1,2).contiguous()
+
+        attention_qkv = attention_qkv.view(x.shape)
+
+        output = self.W_output(attention_qkv)
+
+        return output
+
+
+
+
+
+
+

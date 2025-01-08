fc43936266fb0407bd82cf3ab56db3ac78179d6c
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2025-01-08 11:03:30 -0600

    added model_converter

--- model_loader.py
+++ model_loader.py
@@ -1,2 +1,15 @@
 from CLIP import CLIP
 from encoder import VAE_encoder
+from decoder import VAE_decoder
+from diffusion import Diffusion
+import model_converter
+
+def preload_models_based_on_std_weights(ckpt_path, device):
+    state_dict = model_converter.load_from_standard_weights(ckpt_path,device)
+
+    encoder = VAE_encoder().to(device)
+    encoder.load_state_dict(state_dict=state_dict["encoder"], strict=True)
+
+
+    decoder = VAE_decoder().to(device)
+    decoder.load_state_dict()

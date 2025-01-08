f393e25a72c8034a951a546293d3f51e674876ca
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2025-01-08 11:07:25 -0600

    completed model loader

--- model_loader.py
+++ model_loader.py
@@ -12,4 +12,17 @@ def preload_models_based_on_std_weights(ckpt_path, device):
 
 
     decoder = VAE_decoder().to(device)
-    decoder.load_state_dict()
+    decoder.load_state_dict(state_dict=state_dict["decoder"], strict=True)
+
+    diffusion = Diffusion().to(device)
+    diffusion.load_state_dict(state_dict=state_dict["diffusion"], strict=True)
+
+    clip = CLIP().to(device)
+    clip.load_state_dict(state_dict=state_dict["clip"], strict=True)
+
+    return {
+        "clip": clip,
+        "encoder": encoder,
+        "decoder": decoder,
+        "diffusion": diffusion
+    }
